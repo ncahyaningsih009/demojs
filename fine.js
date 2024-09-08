3f8fcac372230132842c98de61c037758228e3c8
ncahyaningsih009 <ncahyaningsih009@gmail.com>
2024-09-08 10:54:28 +0800

    Update fine.js

--- fine.js
+++ fine.js
@@ -3,7 +3,7 @@
 function search_animal() {
   let input = document.getElementById('searchbar').value
   input = input.toLowerCase();
-  let x = document.getElementsByClassName('animals');
+  let x = document.getElementsByClassName('panggilan');
 
   for (i = 0; i < x.length; i++) {
     if (!x[i].innerHTML.toLowerCase().includes(input)) {
