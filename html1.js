629c77a9b234d21e766e1c86c73d4d002ffbf8ca
ncahyaningsih009 <ncahyaningsih009@gmail.com>
2024-09-08 10:14:23 +0800

    Update html1.js

--- html1.js
+++ html1.js
@@ -43,3 +43,32 @@ const menu2 = `
                placeholder="cari nama permainan...">
 `;
 
+const menu3 = `
+<div class="menu1">
+    <a href="./game/pragmatic.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/pragmatic.png"></a>
+    <a href="./game/pgsoft.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/pgsoft.png"></a>
+    <a href="./game/spadegaming.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/spadegaming.png"></a>
+    <a href="./game/fastspin.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/fastspin.png"></a>
+    <a href="./game/joker.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/joker.png"></a>
+    <a href="./game/boongo.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/boongo.png"></a>
+    <a href="./game/jili.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/jili.png"></a>
+    <a href="./game/playtech.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/playtech.png"></a>
+    <a href="./game/microgaming.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/microgaming.png"></a>
+    <a href="./game/habanero.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/habanero.png"></a>
+    <a href="./game/cq9.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/cq9.png"></a>
+    <a href="./game/playstar.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/playstar.png"></a>
+    <a href="./game/yggdrasil.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/yggdrasil.png"></a>
+    <a href="./game/netent.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/netent.png"></a>
+    <a href="./game/skywind.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/skywind.png"></a>
+    <a href="./game/toptrendgaming.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/toptrendgaming.png"></a>
+    <a href="./game/nextspin.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/nextspin.png"></a>
+    <a href="./game/bbin.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/bbin.png"></a>
+    <a href="./game/creativegaming.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/creativegaming.png"></a>
+    <a href="./game/evoplay.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/evoplay.png"></a>
+    <a href="./game/royalgaming.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/royalgaming.png"></a>
+    <a href="./game/fishing.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/fishing.png"></a>
+    <a href="./game/arcade.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/arcade.png"></a>
+    <a href="./game/casino.html" class="stay tmenu1"><img class="gmenu1" src="./game/icon/casino.png"></a>
+  </div>
+`;
+
